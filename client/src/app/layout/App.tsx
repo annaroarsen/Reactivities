70fe02aa2afa3c3a6813214c3e4fc86766fdfa3d
annaroarsen <annaroarsen@gmail.com>
2025-04-08 10:27:32 +0200

    Funksjonalitet for å vise og lukke en mer detaljert oversikt over aktiviteten

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
 
   useEffect(() => {
     axios.get('https://localhost:5001/api/activities')
@@ -15,12 +16,25 @@ function App() {
     return () => {}
   }, [])
 
+  const handleSelectActivity = (id: string) => {
+    setSelectedActivity(activities.find(x => x.id === id));
+  }
+
+  const handleCancelActivity = () => {
+    setSelectedActivity(undefined);
+  }
+
   return (
     <Box sx={{bgcolor: '#eeeeee'}}>
       <CssBaseline/>
       <NavBar/>
       <Container maxWidth='xl' sx={{mt: 3}}>
-      <ActivityDashboard activities={activities}/>
+      <ActivityDashboard 
+      activities={activities}
+      selectActivity={handleSelectActivity}
+      cancelSelectActivity={handleCancelActivity}
+      selectedActivity={selectedActivity}
+      />
 
       </Container>
       
